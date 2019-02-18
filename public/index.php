f01de9449618088ea4235d3a8a3022845c635e61
Kayla Evans <kae25@njit.edu>
2019-02-18 14:08:06 -0500

    Task: Adding readable csv file

--- public/index.php
+++ public/index.php
@@ -6,3 +6,96 @@
  * Time: 7:21 PM
  */
 
+
+$fileName = 'miniproj.csv';
+main::start($fileName);
+
+class main {
+
+    static public function start($fileName) {
+        $records = csv::getRecords($fileName);
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+
+}
+
+class csv {
+
+    static public function getRecords($fileName) {
+
+        $file = fopen($fileName, "r");
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            $records[] = $record;
+        }
+
+        fclose($file);
+        return $records;
+
+    }
+}
+
+class html{
+
+    static public function generateTable() {}
+}
+
+class system {
+
+    static public function printPage($page) {}
+
+echo $page;
+}
+
+
+COMMIT 1: Feature: Adding readable csv file
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kae25
+ * Date: 2/14/19
+ * Time: 7:21 PM
+ */
+
+
+main::start("miniproj.csv");
+
+class main {
+
+    static public function start($filename) {
+
+        $records = csv::getRecords($filename);
+
+        print_r($records);
+    }
+
+}
+
+class csv {
+
+    static public function getRecords($filename) {
+
+        $file = fopen($filename, "r");
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            $records[] = $record;
+        }
+
+        fclose($file);
+        return $records;
+
+    }
+}
+
+class record {}
+
+class recordFactory {
+
+public static create(Array $array) {}}
